1705ea5739c8571733f65d3c81f706a07a9c5369
BIPIN BABU <bipinbabu@live.com>
2023-08-11 00:26:01 +0100

    Added Notebooks for change detection, CD + AD

--- utils/change_detection.py
+++ utils/change_detection.py
@@ -3,10 +3,12 @@ import utils.helper_functions as hf
 
 
 def perform_change_detection(all_frames_in_batches_dict):
-
     """This function takes in a DICTIONARY of frames in batches
      and perform change detection. Returns a list of keys indicating
       the sequences with significant changes"""
+
+    frame_average_tracker = {}
+    key_list = []
 
     for frame_index, frame_sequence in all_frames_in_batches_dict.items():
         frame_sum = None
@@ -19,4 +21,21 @@ def perform_change_detection(all_frames_in_batches_dict):
                 frame_sum += frame.astype(np.float64)
 
         frame_sequence_average = frame_sum / frame_count
+        frame_average_tracker[frame_index] = frame_sequence_average
 
+    key_list = get_significant_change_frames(frame_average_tracker)
+
+    return key_list
+
+
+def get_significant_change_frames(average_frames_tracker, threshold=0.05):
+    """This function will return the frames with significant changes"""
+
+    significant_change_key_list = []
+
+    for i in range(1, len(average_frames_tracker)):
+        frame_difference = average_frames_tracker[i] - average_frames_tracker[i + 1]
+        if np.mean(frame_difference) > threshold:
+            significant_change_key_list.append(i)
+
+    return significant_change_key_list
--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -1,14 +1,14 @@
-import data_extractor as de
+from utils import data_extractor as de
 import Config as Config
-import data_preprocessing as dp
+from utils import data_preprocessing as dp
 import os
 import numpy as np
 import cv2
 from tqdm.notebook import tqdm
+import matplotlib.pyplot as plt
 
 
 def get_all_frames_from_videos_in_directory():
-
     """This function will return all the frames in the video
      from directory as a LIST with a length as the multiple of provided sequence length, ie disregards
      the remaining frames"""
@@ -73,13 +73,77 @@ def get_dataset_in_sequences():
             yield clip, clip
 
 
+def get_frames_in_batches(batch_size, frames_dict):
+    count = 0
+    frames_in_batch_size = []
+    all_frames_list = []
+
+    for i in range(1, len(frames_dict) + 1):
+        count += 1
+        frame = frames_dict[i]
+        preprocessed_frame = dp.perform_frame_preprocessing(frame)
+        frames_in_batch_size.append(preprocessed_frame)
+
+        if count == batch_size:
+            count = 0
+            all_frames_list.extend(frames_in_batch_size)
+            frames_in_batch_size = []
+
+    all_frames_array = np.array(all_frames_list)
+    num_batches = all_frames_array.shape[0] // batch_size
+    all_frames_in_batches = all_frames_array.reshape((num_batches, batch_size, 256, 256, 1))
+
+    return all_frames_in_batches
+
+
 def get_frame_sequence_tracker(frames_in_batches):
     """This function will return a DICTIONARY with key as the sequence order number
     and value as the sequence itself"""
 
     frame_sequence_tracker = {}
 
-    for count,frame_sequence in enumerate(frames_in_batches):
+    for count, frame_sequence in enumerate(frames_in_batches):
         frame_sequence_tracker[count + 1] = frame_sequence
 
     return frame_sequence_tracker
+
+
+def get_frames_from_video(video_file_path):
+    video_file = cv2.VideoCapture(video_file_path)
+
+    total_frames = 0
+    original_frames_dict = {}
+
+    while True:
+        ret, frame = video_file.read()
+
+        # If the end of frames or any error in reading
+        if not ret:
+            break
+
+        total_frames += 1
+
+        original_frames_dict[total_frames] = frame
+
+    video_file.release()
+    cv2.destroyAllWindows()
+
+    return original_frames_dict
+
+
+def get_original_frame_numbers(sequence_key, batch_size=30):
+    """Returns the start and end original frame numbers for the given sequence key."""
+
+    start_frame = ((sequence_key - 1) * batch_size + 1)
+    end_frame = sequence_key * batch_size
+
+    return start_frame, end_frame
+
+
+def display_image(image):
+    plt.imshow(image)
+    plt.show()
+
+
+def test_func():
+    print('hello world')
